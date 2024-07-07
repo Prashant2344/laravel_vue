4ea228ac16b1391cf36ff5ee1607ba025ad9cec7
prashant-cyberarrow <prashant.silpakar@cyberarrow.io>
2024-07-07 22:54:43 +0545

    User module: list and store user completed

---
+++ resources/js/pages/users/ListUsers.vue
@@ -0,0 +1,126 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, reactive, ref } from 'vue';
+
+const users = ref([]);
+const form = reactive({
+    name: '',
+    email: '',
+    password: ''
+});
+
+const getUsers = () => {
+    axios.get('/api/users')
+        .then((response) => {
+            users.value = response.data
+        })
+}
+
+const createUser = () => {
+    axios.post('/api/users',form)
+    .then((response) => {
+        users.value.unshift(response.data);
+        form.name = '',
+        form.email = '',
+        form.password = '',
+        $('#userFormModal').modal('hide');
+    });
+}
+
+onMounted(() => {
+    getUsers();
+});
+</script>
+<template>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Users</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Users</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="content">
+        <div class="container-fluid">
+            <button type="button" class="mb-2 btn btn-primary" data-toggle="modal" data-target="#userFormModal">
+                Add New User
+            </button>
+            <div class="card">
+                <div class="card-body">
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Registered Date</th>
+                                <th>Role</th>
+                                <th>Options</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="user in users" :key="user.id">
+                                <td>{{ user.id }}</td>
+                                <td>{{ user.name }}</td>
+                                <td>{{ user.email }}</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="userFormModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Add New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form autocomplete="off">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input v-model="form.name" type="text" class="form-control " id="name" aria-describedby="nameHelp"
+                                placeholder="Enter full name">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input v-model="form.email" type="email" class="form-control " id="email" aria-describedby="nameHelp"
+                                placeholder="Enter full name">
+                        </div>
+                    </form>
+
+                    <div class="form-group">
+                        <label for="email">Password</label>
+                        <input v-model="form.password" type="password" class="form-control " id="password" aria-describedby="nameHelp"
+                            placeholder="Enter password">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button @click="createUser" type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
